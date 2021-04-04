f17d7e30c04d42d5b6b0134be7fce29d5cb4d082
hibikon <hibikon.asr.mac@gmail.com>
2021-04-04 12:57:43 +0900

    create second package

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## First
     - 基本的な構文
     - System.out.println / System.out.print
-    
+<br>
+
+## Second
+
+    - Javaの演算
+    - 変数の宣言
---
+++ src/second/Sample2.java
@@ -0,0 +1,40 @@
+package second;
+
+public class Sample2 {
+
+    public static void main(String[] args) {
+
+        /*
+        int → 整数値を宣言
+         */
+
+        // 変数の宣言
+        int a;
+
+        // 初期化と代入を同時に行う
+        int b = 3;
+
+        // 複数の変数を同時に宣言
+        int add, sub;
+
+        // int以外の変数を宣言
+        double avg;
+
+        // 代入（初期化）
+        a = 6;
+
+        // aとbの和を求める
+        add = a + b;
+
+        // aとbの差を求める
+        sub = a - b;
+
+        // aとbの平均値を求める
+        avg = (a + b) / 2.0;
+
+        //出力
+        System.out.println(a + " + " + b + " = " + add);
+        System.out.println(a + " - " + b + " = " + sub);
+        System.out.println(a + " と " + b + " の平均値： " + avg);
+    }
+}
