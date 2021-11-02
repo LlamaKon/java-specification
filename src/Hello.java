129171ff0f31dddea524b37d50b42503e51b53e5
hibikon <hibikon.asr.mac@gmail.com>
2021-11-02 15:37:34 +0900

    commit

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+import test.HelloTest;
+
+public class Hello extends HelloTest {
+
+    public static void main(String[] args) {
+
+        Hello h = new Hello();
+
+        // System.out.println(HelloTest.a);
+
+        // System.out.println(HelloTest.b);
+        System.out.println(h.c);
+        System.out.println(h.d);
+
+
+    }
+}
---
+++ src/test/HelloTest.java
@@ -0,0 +1,12 @@
+package test;
+
+public class HelloTest {
+
+    int a = 0;
+
+    private int b = 0;
+
+    protected int c = 0;
+
+    public int d = 0;
+}
